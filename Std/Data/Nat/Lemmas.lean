0b5dfc10bf8b359ba3b9441047137a42ae55772c
François G. Dorais <fgdorais@gmail.com>
2023-07-04 23:28:32 -0400

    feat: golf dvd

--- Std/Data/Nat/Lemmas.lean
+++ Std/Data/Nat/Lemmas.lean
@@ -689,10 +689,9 @@ protected theorem mul_pow (a b n : Nat) : (a * b) ^ n = a ^ n * b ^ n := by
 
 /-! ### log2 -/
 
-theorem le_log2 (h : n ≠ 0) : k ≤ n.log2 ↔ 2 ^ k ≤ n := by
-  match k with
-  | 0 => simp [show 1 ≤ n from Nat.pos_of_ne_zero h]
-  | k+1 =>
+theorem le_log2 (h : n ≠ 0) : ∀ {k}, k ≤ n.log2 ↔ 2 ^ k ≤ n
+  | 0 => by simp [show 1 ≤ n from Nat.pos_of_ne_zero h]
+  | k+1 => by
     rw [log2]; split
     · have n0 : 0 < n / 2 := (Nat.le_div_iff_mul_le (by decide)).2 ‹_›
       simp [Nat.add_le_add_iff_right, le_log2 (Nat.ne_of_gt n0), le_div_iff_mul_le, Nat.pow_succ]
